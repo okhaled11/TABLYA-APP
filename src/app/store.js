0a44eb41f28217543ecc549ce9db7eb3edd44a07
Atef-mohamed <wmhmdk034@gmail.com>
2025-10-29 23:19:49 +0200

    handle login Auth

--- src/app/store.js
+++ src/app/store.js
@@ -3,23 +3,23 @@ import PersonalRegisterChefReducer from "./features/PersonalRegisterChefSlice";
 import { supabaseApi } from "./features/MenuSlices";
 // import CartSlice from "./CartSlice";
 // import { Posts } from "./PostsSlice";
-export const store = configureStore({
-    reducer: {
-        // CartSlice: persistedReducer,
-        // [Posts.reducerPath]: Posts.reducer,
-        PersonalRegisterChef: PersonalRegisterChefReducer,
-        [supabaseApi.reducerPath]: supabaseApi.reducer,
-    },
+import authReducer from "./features/Auth/loginSlice";
 
-    // middleware: (getDefaultMiddleware) =>
-    //     getDefaultMiddleware({
-    //         serializableCheck: false,
-    //     }).concat([Posts.middleware]),
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(supabaseApi.middleware),
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    // CartSlice: persistedReducer,
+    // [Posts.reducerPath]: Posts.reducer,
+    PersonalRegisterChef: PersonalRegisterChefReducer,
+    [supabaseApi.reducerPath]: supabaseApi.reducer,
+  },
+
+  // middleware: (getDefaultMiddleware) =>
+  //     getDefaultMiddleware({
+  //         serializableCheck: false,
+  //     }).concat([Posts.middleware]),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(supabaseApi.middleware),
 });
 
 export default store;
-
-
-
