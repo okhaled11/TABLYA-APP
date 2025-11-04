ba1019490e6ffe51b981eae523cb96bd9874fd8d
Mariam Hegazy <mariamhegazy24@gmail.com>
2025-11-04 12:01:12 +0200

    Dashboard initially

---
+++ src/components/Admin/Dashboard_page/OrderActivity.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Chart, useChart } from "@chakra-ui/charts"
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
+import {
+    Box,
+    Card,
+    CardBody,
+    CardHeader,
+    Heading,
+} from "@chakra-ui/react";
+
+import { useColorMode } from "../../../theme/color-mode";
+
+export default function OrderActivity() {
+    const chart = useChart({
+        data: [
+            { allocation: 60, type: "Stock" },
+            { allocation: 45, type: "Crypto" },
+            { allocation: 12, type: "ETF" },
+            { allocation: 4, type: "Cash" },
+        ],
+        series: [{ name: "allocation", color: "teal.solid" }],
+    })
+
+
+    const { colorMode } = useColorMode();
+    return (
+
+        <Card.Root
+            bg={colorMode === "light" ? "white" : "rgb(20, 4, 2)"}
+            borderRadius="xl"
+            shadow="sm"
+            border="none"
+
+            my={"20px"}
+            h="500px"
+            w={"100%"}
+        >
+            <CardHeader>
+                <Heading fontSize="18px" fontWeight="semibold">
+                    Weekly Order Activity
+                </Heading>
+            </CardHeader>
+
+            <CardBody>
+                <Box w="100%" h="300px">
+
+                    <Chart.Root maxH="sm" chart={chart}>
+                        <BarChart data={chart.data}>
+                            <CartesianGrid stroke={chart.color("border.muted")} vertical={false} />
+                            <XAxis axisLine={false} tickLine={false} dataKey={chart.key("type")} />
+                            <YAxis
+                                axisLine={false}
+                                tickLine={false}
+                                domain={[0, 100]}
+                                tickFormatter={(value) => `${value}%`}
+                            />
+                            {chart.series.map((item) => (
+                                <Bar
+                                    key={item.name}
+                                    isAnimationActive={false}
+                                    dataKey={chart.key(item.name)}
+                                    fill={chart.color(item.color)}
+                                />
+                            ))}
+                        </BarChart>
+                    </Chart.Root>
+
+                </Box>
+            </CardBody>
+        </Card.Root>
+    )
+}
--- src/pages/AdminPages/Dashboard.jsx
+++ src/pages/AdminPages/Dashboard.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { Box, Heading, HStack, Text } from '@chakra-ui/react'
+
+import Kpis from '../../components/Admin/Dashboard_page/kpis';
+import RevenueChart from '../../components/Admin/Dashboard_page/RevenueChart';
+import CuisinesChart from '../../components/Admin/Dashboard_page/CuisinesChart';
+import OrderActivity from '../../components/Admin/Dashboard_page/OrderActivity';
 
 export default function Dashboard() {
+
+
+
+
   return (
-    <div>Dashboard</div>
+    <Box>
+
+      {/* Dashboard heading */}
+      <Heading as="h1" fontWeight={"semibold"} fontSize={"28px"} my="3">DashBoard Overview </Heading>
+      <Text fontSize={"18px"} color={"GrayText"}>
+        Track your platform's performance and metrics
+
+      </Text>
+
+      {/* KPIS  */}
+
+
+      <Kpis />
+
+   {/* Charts ( Revenue and Cuisines) */}
+
+      <HStack spacing={4}>
+
+        <RevenueChart />
+
+        <CuisinesChart />
+
+
+      </HStack>
+
+     {/* order activity chart */}
+      <OrderActivity />
+
+
+
+    </Box>
   )
 }
